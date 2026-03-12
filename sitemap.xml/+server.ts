e4f5494d7e15d99586b9b63c173c93c3f9493cb0
Gokul Raj R <152710999+Gokul-Raj-R-Coder@users.noreply.github.com>
2026-03-12 12:17:23 +0530

    Create +server.ts

---
+++ sitemap.xml/+server.ts
@@ -0,0 +1,13 @@
+export async function GET() {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://local-converter-6q1j.vercel.app/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+  return new Response(body, {
+    headers: { 'Content-Type': 'application/xml' }
+  });
+}
